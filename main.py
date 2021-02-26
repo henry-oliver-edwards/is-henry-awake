80e661cb383b2dadc350d94520a9110414be0792
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-02-26 15:53:52 +0000

    Updated from pandas to datetime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import gather_keys_oauth2 as Oauth2
 import fitbit
-import pandas as pd
 import datetime
 from time import sleep
 import csv
@@ -15,7 +14,7 @@ REFRESH_TOKEN = str(server.fitbit.client.session.token["refresh_token"])
 
 Henry = fitbit.Fitbit(OAuthCID, ClientSecret, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
 
-startTime = pd.datetime(year=2021, month=2, day=16)
+startTime = datetime.datetime(year=2021, month=2, day=16)
 
 csv_heads = ["ID", "Time", "Steps"]
 
@@ -25,13 +24,16 @@ with open("steps-data.csv", "a") as file:
 
 ID = 0
 
-# print(data)
-# print(data["summary"])
-# print(data["summary"]["steps"])
-
 while True:
     data = Henry.activities()
     steps = data["summary"]["steps"]
     time = datetime.datetime.now()
     print(f"Adding log at ID={ID}, Time={time}, Steps={steps}")
+
+    data_list = [ID, time, steps]
+
+    with open("steps-data.csv", "a") as file:
+        writer = csv.writer(file)
+        writer.writerow(data_list)
+
     sleep(30)
